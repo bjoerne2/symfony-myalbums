31f0bc95d360b4a160892f07511d1e685ca1b7f5
Björn Weinbrenner <bjoern_weinbrenner@yahoo.de>
2013-09-09 13:54:44 +0200

    Add entity Album

---
+++ src/Bjoerne/MyAlbumsBundle/Entity/Album.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Bjoerne\MyAlbumsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Album
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Bjoerne\MyAlbumsBundle\Entity\AlbumRepository")
+ */
+class Album
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="artist", type="string", length=100)
+     */
+    private $artist;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    private $title;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set artist
+     *
+     * @param string $artist
+     * @return Album
+     */
+    public function setArtist($artist)
+    {
+        $this->artist = $artist;
+    
+        return $this;
+    }
+
+    /**
+     * Get artist
+     *
+     * @return string 
+     */
+    public function getArtist()
+    {
+        return $this->artist;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Album
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+}
