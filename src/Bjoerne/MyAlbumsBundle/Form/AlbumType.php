3a7898e714194929a347b263dc9b3a19b3b86957
Björn Weinbrenner <bjoern_weinbrenner@yahoo.de>
2013-09-09 14:00:10 +0200

    Add CRUD controller for Album

---
+++ src/Bjoerne/MyAlbumsBundle/Form/AlbumType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bjoerne\MyAlbumsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AlbumType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('artist')
+            ->add('title')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bjoerne\MyAlbumsBundle\Entity\Album'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bjoerne_myalbumsbundle_album';
+    }
+}
